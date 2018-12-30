5a59ea57264cd7e5d7d6b45d337cb3623f7e78cf
chengs2035 <chensongguo@gmail.com>
2018-12-30 16:17:07 +0800

    first commit;

---
+++ BFOShare/MainForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: cheng
+ * Date: 2018/12/21
+ * Time: 9:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace BFOShare
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.WebBrowser wbForm;
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+		private System.Windows.Forms.ContextMenuStrip barRightMenu;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.wbForm = new System.Windows.Forms.WebBrowser();
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+			this.barRightMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+			this.contextMenuStrip1.SuspendLayout();
+			this.barRightMenu.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// wbForm
+			// 
+			this.wbForm.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.wbForm.Location = new System.Drawing.Point(0, 0);
+			this.wbForm.MinimumSize = new System.Drawing.Size(20, 20);
+			this.wbForm.Name = "wbForm";
+			this.wbForm.Size = new System.Drawing.Size(1270, 763);
+			this.wbForm.TabIndex = 0;
+			this.wbForm.Url = new System.Uri("http://10.169.1.241:8080/qcbin/start_a.jsp", System.UriKind.Absolute);
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "notifyIcon1";
+			this.notifyIcon1.Visible = true;
+			this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1MouseDoubleClick);
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.toolStripMenuItem1,
+			this.toolStripMenuItem2});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(101, 48);
+			this.contextMenuStrip1.Text = "显示";
+			// 
+			// toolStripMenuItem1
+			// 
+			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(100, 22);
+			this.toolStripMenuItem1.Text = "显示";
+			this.toolStripMenuItem1.Click += new System.EventHandler(this.ToolStripMenuItem1Click);
+			// 
+			// toolStripMenuItem2
+			// 
+			this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+			this.toolStripMenuItem2.Size = new System.Drawing.Size(100, 22);
+			this.toolStripMenuItem2.Text = "退出";
+			this.toolStripMenuItem2.Click += new System.EventHandler(this.ToolStripMenuItem2Click);
+			// 
+			// barRightMenu
+			// 
+			this.barRightMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.toolStripMenuItem3,
+			this.toolStripMenuItem4,
+			this.toolStripSeparator1,
+			this.toolStripTextBox1});
+			this.barRightMenu.Name = "barRightMenu";
+			this.barRightMenu.Size = new System.Drawing.Size(193, 79);
+			// 
+			// toolStripMenuItem3
+			// 
+			this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+			this.toolStripMenuItem3.Size = new System.Drawing.Size(192, 22);
+			this.toolStripMenuItem3.Text = "置顶";
+			// 
+			// toolStripMenuItem4
+			// 
+			this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+			this.toolStripMenuItem4.Size = new System.Drawing.Size(192, 22);
+			this.toolStripMenuItem4.Text = "toolStripMenuItem4";
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(189, 6);
+			// 
+			// toolStripTextBox1
+			// 
+			this.toolStripTextBox1.Name = "toolStripTextBox1";
+			this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1270, 763);
+			this.ContextMenuStrip = this.barRightMenu;
+			this.Controls.Add(this.wbForm);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.Text = "ALM Browser BY:djc8.cn";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFormFormClosing);
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.SizeChanged += new System.EventHandler(this.MainFormSizeChanged);
+			this.contextMenuStrip1.ResumeLayout(false);
+			this.barRightMenu.ResumeLayout(false);
+			this.barRightMenu.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ BFOShare/MainForm.cs
@@ -0,0 +1,137 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: cheng
+ * Date: 2018/12/21
+ * Time: 9:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+namespace BFOShare
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		static string inifilepath="localdata.ini";
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void NotifyIcon1MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			if (WindowState == FormWindowState.Minimized) {
+				//还原窗体显示    
+				WindowState = FormWindowState.Normal;
+				//激活窗体并给予它焦点
+				this.Activate();
+				//任务栏区显示图标
+				this.ShowInTaskbar = true;
+				//托盘区图标隐藏
+				notifyIcon1.Visible = false;
+			}
+		}
+
+		void MainFormFormClosing(object sender, FormClosingEventArgs e)
+		{
+			e.Cancel=true;
+			WindowState=FormWindowState.Minimized;
+			this.ShowInTaskbar=false;
+			notifyIcon1.Visible=true;
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			//载入配置文件
+			//先判断是否有配置，如果没有则创建
+			//Localdata.ini
+			
+			if(!File.Exists(inifilepath)){
+				FileStream fs=File.Create(inifilepath);
+				if(fs!=null){
+					fs.Flush();
+					fs.Close();
+					IniHelper.IniHelper.Write(inifilepath,"WinFormIni","url","http://www.djc8.cn","页面url地址的");
+					IniHelper.IniHelper.Write(inifilepath,"WinFormIni","width","1300","窗口宽度");
+					IniHelper.IniHelper.Write(inifilepath,"WinFormIni","heigth","700","窗口高度");
+					IniHelper.IniHelper.Write(inifilepath,"WinFormIni","title","Sharep ALM Browser","窗口标题");
+				}
+			}
+			
+			List<IniHelper.IniStruct> iniValues= IniHelper.IniHelper.ReadValues(inifilepath);
+			string url=IniHelper.IniHelper.ReadValue(inifilepath,"url","WinFormIni");
+			string width=IniHelper.IniHelper.ReadValue(inifilepath,"width","WinFormIni");
+			string heigth=IniHelper.IniHelper.ReadValue(inifilepath,"heigth","WinFormIni");
+			string title=IniHelper.IniHelper.ReadValue(inifilepath,"title","WinFormIni");
+			if(width!="" && heigth!=""){
+				this.Width=Int32.Parse(width);
+				this.Height=Int32.Parse(heigth);
+			}
+			if(title!=""){
+				this.Text=title;
+			}
+			if(url!=""){
+				this.wbForm.Url=new System.Uri(url);
+			}
+		}
+		void saveIni(){
+			//IniHelper.IniHelper.Write(inifilepath,"WinFormIni","url","http://www.djc8.cn","页面url地址的");
+			IniHelper.IniHelper.Write(inifilepath,"WinFormIni","width",this.Width.ToString(),"窗口宽度");
+			IniHelper.IniHelper.Write(inifilepath,"WinFormIni","heigth",this.Height.ToString(),"窗口高度");
+			IniHelper.IniHelper.Write(inifilepath,"WinFormIni","title","Sharep ALM Browser","窗口标题");
+		}
+		void MainFormSizeChanged(object sender, EventArgs e)
+		{
+			//判断是否选择的是最小化按钮
+            if (WindowState == FormWindowState.Minimized)
+            {
+                //隐藏任务栏区图标
+                this.ShowInTaskbar = false;
+                //图标显示在托盘区
+                notifyIcon1.Visible = true;
+            }
+            else{
+            	//保存下当前配置
+            	saveIni();
+            }
+            
+		}
+		/**
+		 * 显示
+		 */
+		void ToolStripMenuItem1Click(object sender, EventArgs e)
+		{
+			if (WindowState == FormWindowState.Minimized) {
+				//还原窗体显示    
+				WindowState = FormWindowState.Normal;
+				//激活窗体并给予它焦点
+				this.Activate();
+				//任务栏区显示图标
+				this.ShowInTaskbar = true;
+				//托盘区图标隐藏
+				notifyIcon1.Visible = false;
+			}
+		}
+		void ToolStripMenuItem2Click(object sender, EventArgs e)
+		{
+			if (MessageBox.Show("是否确认退出程序？", "退出", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                // 关闭所有的线程
+                this.Dispose();
+                this.Close();
+            }
+            
+		}
+	}
+}
